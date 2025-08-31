1188f9eceb95db010cbc53b5f59d46c9676fc247
MUHAMMAD-SHEHARYAR1 <149948237+MUHAMMAD-SHEHARYAR1@users.noreply.github.com>
2025-08-31 22:46:56 +0500

    Add files via upload

---
+++ Project2/app.py
@@ -0,0 +1,7 @@
+# For Gender 1 --> female and 0 --> male
+# For Churn  1 --> Yes and 0 --> No
+# scaler is exported as scaler.pkl
+# model is exported as model.pkl
+# order of x --> 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
+
+
